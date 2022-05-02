5a20ecd0a23bf375cb3c9e1f5beb52120e3de0d9
Dmitry <dvpavlov555@mail.ru>
2022-05-02 22:09:43 +0300

    Сделал несколько механик
    Сделал отскакивание шарика от стен
    Сделал меню с 2-я кнопками (Старт и выход)

--- ArcanoidUnity/Assets/Arcade/Player/Scripts/Ball.cs
+++ ArcanoidUnity/Assets/Arcade/Player/Scripts/Ball.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public float Speed;
 
     void Start()
     {
-        GetComponent<Rigidbody>().AddForce(new Vector3(100, 0, 100));
+        Respawn();
+        //GetComponent<Rigidbody>().AddForce(new Vector3(100, 0, 100));
     }
 
     private void OnTriggerEnter(Collider other)
@@ -15,5 +17,11 @@ public class Ball : MonoBehaviour
             Destroy(other.gameObject);
 
         }
+    }
+
+    public void Respawn()
+    {
+        transform.position = Vector3.zero;
+        GetComponent<Rigidbody>().velocity = Random.insideUnitSphere.normalized * Speed;
     }
 }
