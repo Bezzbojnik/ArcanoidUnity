e4ee06ba58a8df6e26220340fcfb5d6047aed8e0
Dmitry <dvpavlov555@mail.ru>
2022-06-26 20:09:53 +0300

    Исправил баги с отскоками
    Исправил баги с отскоками и подредактировал границы смещения платформы игрока

--- ArcanoidUnity/Assets/Arcade/Player/Scripts/Ball.cs
+++ ArcanoidUnity/Assets/Arcade/Player/Scripts/Ball.cs
@@ -4,24 +4,38 @@ public class Ball : MonoBehaviour
 {
     public float Speed;
 
+    private Rigidbody _rb;
+    private Vector3 _velocity;
+
     void Start()
     {
-        Respawn();
-        //GetComponent<Rigidbody>().AddForce(new Vector3(100, 0, 100));
-    }
+        _rb = this.GetComponent<Rigidbody>();
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.tag == "Enemy")
-        {
-            Destroy(other.gameObject);
+        _velocity = new Vector3(1 * Speed, 1 * Speed, 1 * Speed);
+        Debug.Log(_velocity);
+        _rb.AddForce(_velocity, ForceMode.VelocityChange);
 
-        }
+        //Respawn();
     }
 
     public void Respawn()
     {
         transform.position = Vector3.zero;
-        GetComponent<Rigidbody>().velocity = Random.insideUnitSphere.normalized * Speed;
+        GetComponent<Rigidbody>().velocity = Random.insideUnitSphere.normalized;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        ReflectProjectile(_rb, collision.contacts[0].normal);
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject.gameObject);
+        }
+    }
+
+    private void ReflectProjectile(Rigidbody rb, Vector3 reflectVector)
+    {
+        _velocity = Vector3.Reflect(_velocity, reflectVector);
+        _rb.velocity = _velocity;
     }
 }
